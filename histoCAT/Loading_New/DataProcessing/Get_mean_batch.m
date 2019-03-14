5f7a501105d67e15e4ba02179d52d30838c6c2a1
DenisSch <captaincolden@googlemail.com>
2019-03-14 15:50:12 -0400

    Adapt to output structure

--- histoCAT/Loading_New/DataProcessing/Get_mean_batch.m
+++ histoCAT/Loading_New/DataProcessing/Get_mean_batch.m
@@ -1,9 +1,10 @@
-function [get_mean,get_mean_name] = Get_mean_batch(marker_position)
+function [get_mean,get_mean_name] = Get_mean_batch(marker_position,...
+    sessionData_name, sessionData_folder)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
 % Load session
-load session.mat
+load(sessionData_name)
 
 % Get global variables
 global Sample_Set_arranged
@@ -15,12 +16,14 @@ Current_Mask = Mask_all.Image;
 %get mean expression for multipage tiff
 global tiff_name
 large_tiff_location = fullfile(Sample_Set_arranged{1,1},tiff_name);
-get_mean = struct2array(regionprops(Current_Mask, imread(large_tiff_location,marker_position), 'MeanIntensity'))';
+get_mean = struct2array(regionprops(Current_Mask, ...
+    imread(large_tiff_location,marker_position), 'MeanIntensity'))';
 %Check to make sure it is a string
 get_mean_name_onlyname = (table2cell(Marker_list(marker_position,1)));
 get_mean_name = strcat('Cell_',get_mean_name_onlyname(1,1));
 
-save(strcat(char(get_mean_name),'.mat'),'get_mean','get_mean_name');
+save(fullfile(sessionData_folder,strcat(char(get_mean_name),'.mat')),...
+    'get_mean','get_mean_name');
 
 end
 
