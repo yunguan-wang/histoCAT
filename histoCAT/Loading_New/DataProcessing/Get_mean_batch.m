6370ba179e6f9406319f390c34c74a6a4b68eb2a
DenisSch <captaincolden@googlemail.com>
2019-03-29 15:00:17 -0400

    WIP

--- histoCAT/Loading_New/DataProcessing/Get_mean_batch.m
+++ histoCAT/Loading_New/DataProcessing/Get_mean_batch.m
@@ -1,4 +1,4 @@
-function [get_mean,get_mean_name] = Get_mean_batch(marker_position)
+function [get_mean,get_mean_name] = Get_mean_batch(marker_position,sessionData_name)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
