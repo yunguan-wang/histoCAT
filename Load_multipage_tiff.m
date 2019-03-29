f375ac58c0fc554521a142f741833d78036a0ced
DenisSch <captaincolden@googlemail.com>
2019-03-29 14:51:05 -0400

    WIP

--- Load_multipage_tiff.m
+++ Load_multipage_tiff.m
@@ -17,15 +17,8 @@ end
 
 %Store marker names for multipage tiffs
 % global just for batch mode
-global Marker_CSV
-if isempty(Marker_CSV) == 0
-    Tiff_name_raw = (table2cell(readtable(Marker_CSV,'ReadVariableNames',false)))';
-else
-    [file_marker_list,path_marker_list] = uigetfile('*.csv');
-    Tiff_name_raw = (table2cell(readtable(strcat(path_marker_list,file_marker_list),'ReadVariableNames',false)))';
-end
-
-
+global Marker_list
+Tiff_name_raw = (table2cell(Marker_list));
 
 %Store marker names similar to single tiff images
 for k=1:size(Tiff_name_raw,2)
